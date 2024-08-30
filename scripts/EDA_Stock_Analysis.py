922cce5225cb8a79a666153e6f1909417c0d0d7c
GetieBalew24 <getiebalew2020@gmail.com>
2024-08-30 08:10:52 +0300

    Remove unnamed colum and check code quality

--- scripts/EDA_Stock_Analysis.py
+++ scripts/EDA_Stock_Analysis.py
@@ -17,3 +17,25 @@ class TextAnalysis:
     def load_data(self):
         self.data = pd.read_csv(self.file_path)
         return self.data
+
+    def check_data_quality(self):
+        # Check for missing values
+        missing_values = self.data.isnull().sum()
+        print("Missing values:")
+        print(missing_values)
+
+        # Check for duplicates
+        duplicates = self.data.duplicated().sum()
+        print("\n Number of duplicates:", duplicates)
+
+        # Check data types
+        data_types = self.data.dtypes
+        print("\n Data types:")
+        print(data_types)
+        
+        # Drops the 'Unnamed' column from a pandas DataFrame
+    def drop_unnamed_column(self):
+        
+        self.data = self.data.drop(['Unnamed: 0'], axis=1)
+        return self.data
+    
