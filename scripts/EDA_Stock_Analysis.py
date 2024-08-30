66729dfaba5011e81878b93baedc44c88e85cefd
GetieBalew24 <getiebalew2020@gmail.com>
2024-08-30 07:09:42 +0300

    load raw analyst rating data

---
+++ scripts/EDA_Stock_Analysis.py
@@ -0,0 +1,19 @@
+import re
+
+import matplotlib.pyplot as plt
+import nltk
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from nltk.corpus import stopwords
+from sklearn.decomposition import LatentDirichletAllocation
+from sklearn.feature_extraction.text import TfidfVectorizer
+from textblob import TextBlob
+
+
+class TextAnalysis:
+    def __init__(self, file_path):
+        self.file_path = file_path
+    def load_data(self):
+        self.data = pd.read_csv(self.file_path)
+        return self.data
